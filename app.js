fc891ff039639f189b185a3f3ab194cdeb006292
Elertan <elertan@Elertans-MacBook-Pro.local>
2016-02-10 09:35:20 +0100

    Test change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello World! Patrick');
 });
 
 app.listen(process.env.PORT || 1337, function () {
