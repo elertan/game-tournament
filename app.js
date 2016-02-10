74a2e689b6ba10f2f1aa1fc549f305c2daacb9f3
Elertan <elertan@Elertans-MacBook-Pro.local>
2016-02-10 01:26:14 +0100

    Edited gitignore added app.js with default code and imported a typings file for visual studio code

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || 1337, function () {
+    console.log('Server is running!');
+});
