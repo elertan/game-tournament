041fcf1471b14172786f18665922dc026dad7269
Elertan <elertan@Elertans-MacBook-Pro.local>
2016-02-10 13:37:26 +0100

    Last

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    console.log('Changed');
+    res.send('Hello World!');
 });
 
 app.listen(process.env.PORT || 1337, function() {
