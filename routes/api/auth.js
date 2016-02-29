c1eb902702b059acb117ad1209422fedb42be773
Elertan <denkievits@gmail.com>
2016-02-29 09:25:55 +0100

    Added existing email check

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,16 +7,24 @@ const paramCheck = require('../../middleware/requiredPostParams');
 const User = require('../../models/user');
 
 router.post('/register', paramCheck(['email', 'password']), function (req, res) {
-	const user = new User();
-	user.email = req.body.email;
-	
-	user.save(function (err, result) {
-		if (err) {
-			res.json({ err: 'User could not be saved' });
+	// Check pre-existing user
+	User.find({ email: req.body.email }, function (err, result) {
+		if (result.length > 0) {
+			res.json({ err: 'User with given email already exists' });
 			return;
 		}
 		
-		res.json({ user: user });
+		const user = new User();
+		user.email = req.body.email;
+		
+		user.save(function (err, result) {
+			if (err) {
+				res.json({ err: 'User could not be saved' });
+				return;
+			}
+			
+			res.json({ user: user });
+		});
 	});
 });
 
