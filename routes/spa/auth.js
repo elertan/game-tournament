5ee46b593a35fa949a704a15680e7802ad87a748
Elertan <denkievits@gmail.com>
2016-03-02 14:32:05 +0100

    Added mail support

--- routes/spa/auth.js
+++ routes/spa/auth.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const router = express.Router();
 
+const mailgun = require('mailgun-js')({ apiKey: 'key-95c69f24df3cf38a009998e4dcc8bb24', domain: 'sandbox18d026f44d7d4065b80d49564681004e.mailgun.org' });
+
 // Add routes here
 router.get('/login', function (req, res) {
 	res.render('spa/auth/login');
@@ -20,12 +22,25 @@ router.post('/register', function (req, res) {
 		return;
 	}
 	
-	if (form.studentnumber.length < 8) {
-		res.json({ err: 'Studentnumber must contain 8 numbers' });
+	if (form.studentnumber.length != 8) {
+		res.json({ err: 'Studentnumber must have 8 numbers' });
 		return;
 	}
 	
-	res.json({ msg: 'There is sent an email to ' + form.studentnumber + '@mydavinci.nl' });
+	const data = {
+		from: 'info@gametournament.nl',
+		to: form.studentnumber + '@mydavinci.nl',
+		subject: 'Account Activatie',
+		text: 'Beste Leerling,\n\nDruk op deze link om jouw account aan te maken http://localhost:1337/#/auth/register/Adu342hda8asdm\n\nMet vriendelijke groet,\n\nHet game tournament team'
+	};
+	
+	mailgun.messages().send(data, function (err, body) {
+		if (!err) {
+			res.json({ msg: 'suc6' });
+		} else {
+			res.json({ err: 'er!!!' });
+		}
+	});
 });
 
 module.exports = router;
