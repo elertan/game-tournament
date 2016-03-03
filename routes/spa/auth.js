34247e4b1a5f068bee1e865a465712da8c1db153
patrickvonk@hotmail.com <patrickvonk@hotmail.com>
2016-03-03 10:22:09 +0100

    commit

--- routes/spa/auth.js
+++ routes/spa/auth.js
@@ -25,7 +25,33 @@ router.post('/register', function (req, res) {
 		return;
 	}
 	
-	res.json({ msg: 'There is sent an email to ' + form.studentnumber + '@mydavinci.nl' });
+	var nodemailer = require('nodemailer');
+	var smtpTransport = require('nodemailer-smtp-transport');
+
+	var transporter = nodemailer.createTransport(smtpTransport({
+		host: 'localhost',
+		port: 25,
+		// auth: {
+		// 	user: 'info@gametournament.nl',
+		// 	pass: ''
+		// }
+	}));
+	
+	// send mail
+	transporter.sendMail({
+		from: 'denkievits@gmail.com',
+		to: 'patrickvonk@hotmail.com',
+		subject: 'hello playground!',
+		text: 'Authenticated with OAuth2'
+	}, function(error, response) {
+	if (error) {
+			console.log(error);
+	} else {
+			console.log('Message sent');
+	}
+	});
+	
+	res.json({ msg: 'An email has been send to ' + form.studentnumber + '@mydavinci.nl' });
 });
 
 module.exports = router;
