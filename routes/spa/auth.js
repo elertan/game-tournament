56403511a8cfbed4c725df452baba9b0b47c4714
Elertan <denkievits@gmail.com>
2016-03-07 10:07:50 +0100

    Added local mailing

--- routes/spa/auth.js
+++ routes/spa/auth.js
@@ -3,7 +3,12 @@
 const express = require('express');
 const router = express.Router();
 
-const mailgun = require('mailgun-js')({ apiKey: 'key-95c69f24df3cf38a009998e4dcc8bb24', domain: 'sandbox18d026f44d7d4065b80d49564681004e.mailgun.org' });
+//const mailgun = require('mailgun-js')({ apiKey: 'key-95c69f24df3cf38a009998e4dcc8bb24', domain: 'sandbox18d026f44d7d4065b80d49564681004e.mailgun.org' });
+const nodemailer = require('nodemailer');
+const randomstring = require('randomstring');
+
+const User = require('../../models/user.js');
+const Registration = require('../../models/registration.js');
 
 // Add routes here
 router.get('/login', function (req, res) {
@@ -18,29 +23,56 @@ router.post('/register', function (req, res) {
 	const form = req.body;
 	
 	if (isNaN(form.studentnumber)) {
-		res.json({ err: 'Studentnumber must be a number' });
+		res.json({ err: 'Studentnummer moet een getal zijn' });
 		return;
 	}
 	
 	if (form.studentnumber.length != 8) {
-		res.json({ err: 'Studentnumber must have 8 numbers' });
+		res.json({ err: 'Studentnummer moet 8 getallen zijn' });
 		return;
 	}
-
-	const data = {
-		from: 'info@gametournament.nl',
-		to: form.studentnumber + '@mydavinci.nl',
-		subject: 'Account Activatie',
-		text: 'Beste Leerling,\n\nDruk op deze link om jouw account aan te maken http://localhost:1337/#/auth/register/Adu342hda8asdm\n\nMet vriendelijke groet,\n\nHet game tournament team'
-	};
 	
-	mailgun.messages().send(data, function (err, body) {
-		if (!err) {
-			res.json({ msg: 'suc6' });
-		} else {
-			res.json({ err: 'er!!!' });
+	const email = form.studentnumber + '@mydavinci.nl';
+	const studentnumber = parseInt(form.studentnumber);
+
+	Registration.findOne({ studentnumber: studentnumber }, function (err, registration) {
+		if (registration) {
+			return;
 		}
+		
+		const data = {
+			from: 'info@gametournament.nl',
+			to: email,
+			subject: 'Account Activatie',
+			text: 'Beste Leerling,\n\nDruk op deze link om jouw account aan te maken http://localhost:1337/#/auth/register/Adu342hda8asdm\n\nMet vriendelijke groet,\n\nHet game tournament team'
+		};
+		
+		const transporter = nodemailer.createTransport({
+			host: 'localhost',
+			port: 9998
+		});
+		
+		transporter.sendMail(data, function (err, info) {
+			if (!err) {
+				const code = randomstring.generate();
+				
+				const registration = new Registration({
+					studentnumber: studentnumber,
+					verificationCode: code
+				});
+				
+				res.json({ msg: 'Er is een email naar ' + email + ' gestuurd.' });
+			} else {
+				res.json({ err: 'Er is een fout opgetreden, probeer het later overnieuw.' });
+			}
+		});
 	});
+});
+
+router.get('/register/:verificationCode', function (req, res) {
+	const code = req.params.verificationCode;
+	
+	
 });
 
 module.exports = router;
